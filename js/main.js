28830857ddc1bc350293cc65bd9692da9fa52095
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-09-12 16:26:02 +0800

    test

--- js/main.js
+++ js/main.js
@@ -110,75 +110,75 @@ var main = function () {
         });
 
         app.io.route("i.am", function (req) {
-            if (storedData.store.get(req.data.who) != null) {
+            if (storedData.store.get(req.data.whoAmI) != null) {
                 req.io.respond({
                     status: false,
-                    message: "'i.am' not accepted"
+                    message: "' (" + req.data.who + ") i.am (" + req.data.whoAmI + ")' not accepted"
                 });
             }
             else {
                 req.io.respond({
                     status: true,
-                    message: "'i.am' accepted"
+                    message: "' (" + req.data.who + ") i.am (" + req.data.whoAmI + ")' accepted"
                 });
 
                 storedData.store.put({
-                    "who": req.data.who,
+                    "who": req.data.whoAmI,
                     "when": req.data.when
                 });
 
-                req.io.join(req.data.who);
+                req.io.join(req.data.whoAmI);
 
                 if (storedData.store.get("Resource Monitor") != null) {
                     app.io.room("Resource Monitor").broadcast("someone.joined", {
-                        who: req.data.who,
+                        who: req.data.whoAmI,
                         when: req.data.when
                     });
                 }
 
                 req.io.broadcast("he.is", {
-                    who: req.data.who,
+                    who: req.data.whoAmI,
                     when: req.data.when
                 });
 
                 req.io.emit("you.are", {
-                    who: req.data.who,
+                    who: req.data.whoAmI,
                     when: req.data.when
                 });
             }
         });
 
         app.io.route("i.am.no.more", function (req) {
-            if (storedData.store.get(req.data.who) == null) {
+            if (storedData.store.get(req.data.whoAmI) == null) {
                 req.io.respond({
                     status: false,
-                    message: "'i.am.no.more' not accepted"
+                    message: "' (" + req.data.who + ") i.am.no.more (" + req.data.whoAmI + ")' not accepted"
                 });
             }
             else {
                 req.io.respond({
                     status: true,
-                    message: "'i.am.no.more' accepted"
+                    message: "' (" + req.data.who + ") i.am.no.more (" + req.data.whoAmI + ")' accepted"
                 });
 
-                storedData.store.remove(req.data.who);
+                storedData.store.remove(req.data.whoAmI);
 
-                req.io.leave(req.data.who);
+                req.io.leave(req.data.whoAmI);
 
                 if (storedData.store.get("Resource Monitor") != null) {
                     app.io.room("Resource Monitor").broadcast("someone.left", {
-                        who: req.data.who,
+                        who: req.data.whoAmI,
                         when: req.data.when
                     });
                 }
 
                 req.io.broadcast("he.is.no.more", {
-                    who: req.data.who,
+                    who: req.data.whoAmI,
                     when: req.data.when
                 });
 
                 req.io.emit("you.are.no.more", {
-                    who: req.data.who,
+                    who: req.data.whoAmI,
                     when: req.data.when
                 });
             }
@@ -187,7 +187,7 @@ var main = function () {
         app.io.route("heartbeat", function (req) {
             req.io.respond({
                 status: true,
-                message: "'heartbeat' accepted"
+                message: "'heartbeat (" + req.data.who + ")' accepted"
             });
 
             if (storedData.store.get(req.data.who) != null) {
@@ -219,13 +219,13 @@ var main = function () {
             if (storedData.store.get(req.data.whom) == null) {
                 req.io.respond({
                     status: false,
-                    message: "'tell.someone' not accepted"
+                    message: "'tell.someone (" + req.data.whom + ")' not accepted"
                 });
             }
             else {
                 req.io.respond({
                     status: true,
-                    message: "'tell.someone' accepted"
+                    message: "'tell.someone (" + req.data.whom + ")' accepted"
                 });
 
                 app.io.room(req.data.whom).broadcast("someone.said", {
