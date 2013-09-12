c5f4361b89e3e89e1d7ccb3df4f1e3ddcda41967
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-09-12 14:31:55 +0800

    test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,73 @@
+Date.prototype.yyyyMMddHHmmss = function () {
+    var date = this;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+
+    return "" + year +
+    (month < 10 ? "0" + month : month) +
+    (day < 10 ? "0" + day : day) +
+    (hh < 10 ? "0" + hh : hh) +
+    (mm < 10 ? "0" + mm : mm) +
+    (ss < 10 ? "0" + ss : ss);
+};
+
+Date.prototype.dateFormat = function () {
+    var date = this;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+
+    return "" + year + "-" +
+    (month < 10 ? "0" + month : month) + "-" +
+    (day < 10 ? "0" + day : day) + " " +
+    (hh < 10 ? "0" + hh : hh) + ":" +
+    (mm < 10 ? "0" + mm : mm) + ":" +
+    (ss < 10 ? "0" + ss : ss);
+};
+
+String.prototype.dateFormat = function () {
+    var that = this.toString();
+
+    var date = new Date(that.substring(0, 4), that.substring(4, 6) - 1, that.substring(6, 8), that.substring(8, 10), that.substring(10, 12), that.substring(12));
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+
+    return "" + year + "-" +
+    (month < 10 ? "0" + month : month) + "-" +
+    (day < 10 ? "0" + day : day) + " " +
+    (hh < 10 ? "0" + hh : hh) + ":" +
+    (mm < 10 ? "0" + mm : mm) + ":" +
+    (ss < 10 ? "0" + ss : ss);
+};
+
+Number.prototype.dateFormat = function () {
+    var date = new Date(this);
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+
+    return "" + year + "-" +
+    (month < 10 ? "0" + month : month) + "-" +
+    (day < 10 ? "0" + day : day) + " " +
+    (hh < 10 ? "0" + hh : hh) + ":" +
+    (mm < 10 ? "0" + mm : mm) + ":" +
+    (ss < 10 ? "0" + ss : ss);
+};
+
 var main = function () {
     require([
         "dojo/node!util",
@@ -30,7 +100,7 @@ var main = function () {
         app.io.on("connection", function (socket) {
             var heartbeat = setInterval(function () {
                 socket.emit("heartbeat", {
-                    when: new Date().getTime()
+                    when: new Date().yyyyMMddHHmmss()
                 });
             }, 60000)
 
@@ -40,64 +110,83 @@ var main = function () {
         });
 
         app.io.route("i.am", function (req) {
-            req.io.respond({
-                message: "'i.am' accepted"
-            });
+            if (storedData.store.get(req.data.who) != null) {
+                req.io.respond({
+                    status: false,
+                    message: "'i.am' not accepted"
+                });
+            }
+            else {
+                req.io.respond({
+                    status: true,
+                    message: "'i.am' accepted"
+                });
 
-            storedData.store.put({
-                "who": req.data.who,
-                "when": req.data.when
-            });
+                storedData.store.put({
+                    "who": req.data.who,
+                    "when": req.data.when
+                });
 
-            req.io.join(req.data.who);
+                req.io.join(req.data.who);
 
-            if (storedData.store.get("Resource Monitor") != null) {
-                app.io.room("Resource Monitor").broadcast("someone.joined", {
+                if (storedData.store.get("Resource Monitor") != null) {
+                    app.io.room("Resource Monitor").broadcast("someone.joined", {
+                        who: req.data.who,
+                        when: req.data.when
+                    });
+                }
+
+                req.io.broadcast("he.is", {
+                    who: req.data.who,
+                    when: req.data.when
+                });
+
+                req.io.emit("you.are", {
                     who: req.data.who,
                     when: req.data.when
                 });
             }
-
-            req.io.broadcast("he.is", {
-                who: req.data.who,
-                when: req.data.when
-            });
-
-            req.io.emit("you.are", {
-                who: req.data.who,
-                when: req.data.when
-            });
         });
 
         app.io.route("i.am.no.more", function (req) {
-            req.io.respond({
-                message: "'i.am.no.more' accepted"
-            });
+            if (storedData.store.get(req.data.who) == null) {
+                req.io.respond({
+                    status: false,
+                    message: "'i.am.no.more' not accepted"
+                });
+            }
+            else {
+                req.io.respond({
+                    status: true,
+                    message: "'i.am.no.more' accepted"
+                });
 
-            storedData.store.remove(req.data.who);
+                storedData.store.remove(req.data.who);
 
-            req.io.leave(req.data.who);
+                req.io.leave(req.data.who);
 
-            if (storedData.store.get("Resource Monitor") != null) {
-                app.io.room("Resource Monitor").broadcast("someone.left", {
+                if (storedData.store.get("Resource Monitor") != null) {
+                    app.io.room("Resource Monitor").broadcast("someone.left", {
+                        who: req.data.who,
+                        when: req.data.when
+                    });
+                }
+
+                req.io.broadcast("he.is.no.more", {
+                    who: req.data.who,
+                    when: req.data.when
+                });
+
+                req.io.emit("you.are.no.more", {
                     who: req.data.who,
                     when: req.data.when
                 });
             }
-
-            req.io.broadcast("he.is.no.more", {
-                who: req.data.who,
-                when: req.data.when
-            });
-
-            req.io.emit("you.are.no.more", {
-                who: req.data.who,
-                when: req.data.when
-            });
         });
 
         app.io.route("heartbeat", function (req) {
             req.io.respond({
+                status: true,
                 message: "'heartbeat' accepted"
             });
 
@@ -115,6 +204,7 @@ var main = function () {
 
         app.io.route("tell.other", function (req) {
             req.io.respond({
+                status: true,
                 message: "'tell.other' accepted"
             });
 
@@ -126,11 +216,18 @@ var main = function () {
         });
 
         app.io.route("tell.someone", function (req) {
-            req.io.respond({
-                message: "'tell.someone' accepted"
-            });
+            if (storedData.store.get(req.data.whom) == null) {
+                req.io.respond({
+                    status: false,
+                    message: "'tell.someone' not accepted"
+                });
+            }
+            else {
+                req.io.respond({
+                    status: true,
+                    message: "'tell.someone' accepted"
+                });
 
-            if (storedData.store.get(req.data.whom) != null) {
                 app.io.room(req.data.whom).broadcast("someone.said", {
                     who: req.data.who,
                     what: req.data.what,
@@ -141,6 +238,7 @@ var main = function () {
 
         app.io.route("who.are.there", function (req) {
             req.io.respond({
+                status: true,
                 message: "'who.are.there' accepted"
             });
 
