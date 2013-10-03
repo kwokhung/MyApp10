f8b1a3dc9b051d8a43e0ec694ff885f01b498a9f
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-10-03 17:43:20 +0800

    test

--- js/app/util/WechatHelper.js
+++ js/app/util/WechatHelper.js
@@ -96,6 +96,11 @@ define([
 
                         break;
 
+                    case "event":
+                        this.handleEvent(req, res);
+
+                        break;
+
                     default:
                         this.handleOther(req, res);
 
@@ -247,6 +252,50 @@ define([
                                     Location_Y: req.body.xml.Location_Y[0],
                                     Scale: req.body.xml.Scale[0],
                                     Label: req.body.xml.Label[0],
+                                    RawData: util.inspect(req.body, false, null)
+                                }) +
+                        "]]>" +
+                    "</Content>" +
+                "</xml>");
+        },
+        handleEvent: function (req, res) {
+            var currentDate = new Date();
+            var currentTime = currentDate.getTime();
+            var currentTimeZone = 0 - currentDate.getTimezoneOffset() / 60;
+
+            var hkDate = currentDate;
+            hkDate.setHours(hkDate.getHours() - currentTimeZone + 8);
+
+            res.send(
+                "<xml>" +
+                    "<ToUserName><![CDATA[" + req.body.xml.FromUserName + "]]></ToUserName>" +
+                    "<FromUserName><![CDATA[" + req.body.xml.ToUserName + "]]></FromUserName>" +
+                    "<CreateTime>" + Math.round(currentTime / 1000) + "</CreateTime>" +
+                    "<MsgType><![CDATA[" + "text" + "]]></MsgType>" +
+                    "<Content>" +
+                        "<![CDATA[" +
+                            string.substitute(
+                                "\n" +
+                                "Current Time: ${CurrentTime}\n\n" +
+                                "Current Time Zone: ${CurrentTimeZone}\n\n" +
+                                "HK Time: ${HkTime}\n\n" +
+                                "Message type: ${MsgType}\n\n" +
+                                "Create Time: ${CreateTime}\n\n" +
+                                "From User: ${FromUserName}\n\n" +
+                                "To User: ${ToUserName}\n\n" +
+                                "Event: ${Event}\n\n" +
+                                "Event Key: ${EventKey}\n\n" +
+                                "Raw Data: ${RawData}",
+                                {
+                                    CurrentTime: currentTime.dateFormat(),
+                                    CurrentTimeZone: currentTimeZone,
+                                    HkTime: hkDate.getTime().dateFormat(),
+                                    MsgType: req.body.xml.MsgType[0],
+                                    CreateTime: (parseInt(req.body.xml.CreateTime[0]) * 1000).dateFormat(),
+                                    FromUserName: req.body.xml.FromUserName[0],
+                                    ToUserName: req.body.xml.ToUserName[0],
+                                    Event: req.body.xml.Event[0],
+                                    EventKey: req.body.xml.EventKey[0],
                                     RawData: util.inspect(req.body, false, null)
                                 }) +
                         "]]>" +
