998d651408e805e9be6d7e757d5972163a79cf8d
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-10-03 14:33:37 +0800

    test

--- js/app/util/WechatHelper.js
+++ js/app/util/WechatHelper.js
@@ -86,7 +86,7 @@ define([
                         "<FromUserName><![CDATA[" + req.body.xml.ToUserName + "]]></FromUserName>" +
                         "<CreateTime>" + Math.round(new Date().getTime() / 1000) + "</CreateTime>" +
                         "<MsgType><![CDATA[" + req.body.xml.MsgType + "]]></MsgType>" +
-                        "<Content><![CDATA[<" + req.body.xml.MsgId + "@" + (parseInt(req.body.xml.CreateTime) * 1000).dateFormat() + "@" + (new Date().getTime()).dateFormat() + ">:<" + req.body.xml.FromUserName + "(" + req.body.xml.Content + ")" + req.body.xml.ToUserName + ">]]></Content>" +
+                        "<Content><![CDATA[<" + req.body.xml.MsgId + "@" + (parseInt(req.body.xml.CreateTime) * 1000).dateFormat() + "@" + (new Date().getTime()).dateFormat() + "+" + (new Date().getTimezoneOffset()).toString() + ">:<" + req.body.xml.FromUserName + "(" + req.body.xml.Content + ")" + req.body.xml.ToUserName + ">]]></Content>" +
                     "</xml>");
             }
             else {
