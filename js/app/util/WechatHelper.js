36e5e6ef4aed8851c76e1b68134c0114e6306457
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-10-07 11:51:19 +0800

    test

--- js/app/util/WechatHelper.js
+++ js/app/util/WechatHelper.js
@@ -231,7 +231,7 @@
             }));
         },
         handleVideo: function (now, req, res) {
-            res.send(this.renderVideo({
+            res.send(this.renderArticle({
                 ToUserName: req.body.xml.FromUserName,
                 FromUserName: req.body.xml.ToUserName,
                 CreateTime: Math.round(now.time / 1000),
@@ -261,7 +261,50 @@
                         RawData: util.inspect(req.body, false, null)
                     }),
                 MediaId: req.body.xml.MediaId[0],
-                ThumbMediaId: req.body.xml.ThumbMediaId[0]
+                ThumbMediaId: req.body.xml.ThumbMediaId[0],
+                Articles: [{
+                    Title: "Video",
+                    Description: "",
+                    PicUrl: "",
+                    Url: ""
+                }, {
+                    Title: string.substitute(
+                        "Current Time: ${CurrentTime}\n\n" +
+                        "Current Time Zone: ${CurrentTimeZone}\n\n" +
+                        "HK Time: ${HkTime}\n\n" +
+                        "Message Id: ${MsgId}\n\n" +
+                        "Message type: ${MsgType}\n\n" +
+                        "Create Time: ${CreateTime}\n\n" +
+                        "From User: ${FromUserName}\n\n" +
+                        "To User: ${ToUserName}", {
+                            CurrentTime: now.time.dateFormat(),
+                            CurrentTimeZone: now.timeZone,
+                            HkTime: now.hkDate.getTime().dateFormat(),
+                            MsgId: (typeof req.body.xml.MsgId == "undefined" ? "" : req.body.xml.MsgId[0]),
+                            MsgType: req.body.xml.MsgType[0],
+                            CreateTime: (parseInt(req.body.xml.CreateTime[0]) * 1000).dateFormat(),
+                            FromUserName: req.body.xml.FromUserName[0],
+                            ToUserName: req.body.xml.ToUserName[0]
+                        }),
+                    Description: "",
+                    PicUrl: "",
+                    Url: ""
+                }, {
+                    Title: string.substitute("Media Id: ${MediaId}", { MediaId: req.body.xml.MediaId[0] }),
+                    Description: "",
+                    PicUrl: "",
+                    Url: ""
+                }, {
+                    Title: string.substitute("ThumbMediaId: ${ThumbMediaId}", { ThumbMediaId: req.body.xml.ThumbMediaId[0] }),
+                    Description: "",
+                    PicUrl: "",
+                    Url: ""
+                }, {
+                    Title: util.inspect(req.body, false, null),
+                    Description: "",
+                    PicUrl: "",
+                    Url: ""
+                }]
             }));
         },
         handleLocation: function (now, req, res) {
@@ -287,7 +330,7 @@
                             CurrentTime: now.time.dateFormat(),
                             CurrentTimeZone: now.timeZone,
                             HkTime: now.hkDate.getTime().dateFormat(),
-                            MsgId: req.body.xml.MsgId[0],
+                            MsgId: (typeof req.body.xml.MsgId == "undefined" ? "" : req.body.xml.MsgId[0]),
                             MsgType: req.body.xml.MsgType[0],
                             CreateTime: (parseInt(req.body.xml.CreateTime[0]) * 1000).dateFormat(),
                             FromUserName: req.body.xml.FromUserName[0],
@@ -347,7 +390,7 @@
                             CurrentTime: now.time.dateFormat(),
                             CurrentTimeZone: now.timeZone,
                             HkTime: now.hkDate.getTime().dateFormat(),
-                            MsgId: req.body.xml.MsgId[0],
+                            MsgId: (typeof req.body.xml.MsgId == "undefined" ? "" : req.body.xml.MsgId[0]),
                             MsgType: req.body.xml.MsgType[0],
                             CreateTime: (parseInt(req.body.xml.CreateTime[0]) * 1000).dateFormat(),
                             FromUserName: req.body.xml.FromUserName[0],
@@ -384,29 +427,6 @@
                 ToUserName: req.body.xml.FromUserName,
                 FromUserName: req.body.xml.ToUserName,
                 CreateTime: Math.round(now.time / 1000),
-                Content: string.substitute(
-                    "\n" +
-                    "Current Time: ${CurrentTime}\n\n" +
-                    "Current Time Zone: ${CurrentTimeZone}\n\n" +
-                    "HK Time: ${HkTime}\n\n" +
-                    "Message type: ${MsgType}\n\n" +
-                    "Create Time: ${CreateTime}\n\n" +
-                    "From User: ${FromUserName}\n\n" +
-                    "To User: ${ToUserName}\n\n" +
-                    "Event: ${Event}\n\n" +
-                    "Event Key: ${EventKey}\n\n" +
-                    "Raw Data: ${RawData}", {
-                        CurrentTime: now.time.dateFormat(),
-                        CurrentTimeZone: now.timeZone,
-                        HkTime: now.hkDate.getTime().dateFormat(),
-                        MsgType: req.body.xml.MsgType[0],
-                        CreateTime: (parseInt(req.body.xml.CreateTime[0]) * 1000).dateFormat(),
-                        FromUserName: req.body.xml.FromUserName[0],
-                        ToUserName: req.body.xml.ToUserName[0],
-                        Event: req.body.xml.Event[0],
-                        EventKey: req.body.xml.EventKey[0],
-                        RawData: util.inspect(req.body, false, null)
-                    }),
                 Articles: [{
                     Title: "Event",
                     Description: "",
